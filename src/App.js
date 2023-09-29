f33c3e495cebe3e886dd6bda2d1fd137d29d7791
Gabriella M Sanchez <83431411+GabriellaMSanchez@users.noreply.github.com>
2023-09-29 13:05:59 -0700

    removed base create-react-app items and started building base trail finder
    Removed base create-react-app items that were not necessary at this time (per online course). Began building base trail-finder text and functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and have fun coding!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Trail Finder</h1>
+            <h4>{new Date().toLocaleDateString()}</h4>
+            <textarea
+                type="text" 
+                placeholder="Search a Park"
+                style={{border: '3px solid black'}}
+                autoFocus
+                spellCheck
+            />
+            <ul>
+                <li>Map of all Parks</li>
+                <li>General Info</li>
+                <li>Park Announcements</li>
+                <li>Calendar Events</li>
+                <li>Random Park</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
