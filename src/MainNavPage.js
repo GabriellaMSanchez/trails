07c46b1af265401d656f2e6f78398b535093f25f
Gabriella M Sanchez <83431411+GabriellaMSanchez@users.noreply.github.com>
2023-10-02 12:37:08 -0700

    Reconfigured MainNavPage text and component
    Reconfigured MainNavPage text and component to match the Mock up better. Removed quote from footer and created real footer.

--- src/MainNavPage.js
+++ src/MainNavPage.js
@@ -1,5 +1,6 @@
 import NavButton from "./NavButton";
 import SearchBar from "./SearchBar";
+import Card from 'react-bootstrap/Card';
 
 function MainNavPage() {
   return (
@@ -9,44 +10,63 @@ function MainNavPage() {
                 <SearchBar />
             </div>
         </div>
-        <div className="text-center"> 
-            <h1>Trail Finder</h1>
-            <h4>Today: {new Date().toLocaleDateString()}</h4>
-        </div>
 
-      <div className="text-center">
-        <ul>
-          <NavButton
-            iconName="fa-solid fa-location-dot"
-            text="Map of all Parks"
-          />
-          <NavButton 
-            iconName="fa-solid fa-circle-info" 
-            text="General Info" />
-          <NavButton
-            iconName="fa-solid fa-bullhorn"
-            text="Park Announcements"
-          />
-          <NavButton
-            iconName="fa-regular fa-calendar-days"
-            text="Calendar Events"
-          />
-          <NavButton
-            iconName="fa-solid fa-bag-shopping"
-            text="Partnered Shops"
-          />
-          <NavButton 
-            iconName="fa-solid fa-dice" 
-            text="Random Park" />
-        </ul>
-      </div>
+        <Card className="main-nav-title-cards main-nav-text" style={{ background: "rgba(0, 0, 0, 0.5)" }}>
+            <Card.Body>
+                <Card.Title className="text-center"> 
+                    <h1>Trail Finder</h1>
+                </Card.Title>
+                <Card.Subtitle className="text-center mb-2 text-muted">
+                    <p>
+                        Find your next adventure. 
+                    </p>
+                </Card.Subtitle>
+                <Card.Subtitle className="text-center mb-2 text-muted">
+                    <p>
+                        Today: {new Date().toLocaleDateString()}
+                    </p>
+                </Card.Subtitle>      
+
+                <Card.Text>
+                    <ul>
+                    <NavButton
+                        iconName="fa-solid fa-location-dot"
+                        text="Map of all Parks"
+                    />
+                    <NavButton 
+                        iconName="fa-solid fa-circle-info" 
+                        text="General Info" />
+                    <NavButton
+                        iconName="fa-solid fa-bullhorn"
+                        text="Park Announcements"
+                    />
+                    <NavButton
+                        iconName="fa-regular fa-calendar-days"
+                        text="Calendar Events"
+                    />
+                    <NavButton
+                        iconName="fa-solid fa-bag-shopping"
+                        text="Partnered Shops"
+                    />
+                    <NavButton 
+                        iconName="fa-solid fa-dice" 
+                        text="Random Park" />
+                    </ul>
+                </Card.Text>
+                <div>
+                    <Card.Text className="text-center">
+                        “Within National Parks is room — glorious room — room in which to find
+                        ourselves, in which to think and hope, to dream and plan, to rest and
+                        resolve.” — Enos Mills
+                    </Card.Text>
+                </div>
+            </Card.Body>
+        </Card>
+
       <footer className="main-nav-footer text-center">
         <p>
-            “Within National Parks is room — glorious room — room in which to find
-            ourselves, in which to think and hope, to dream and plan, to rest and
-            resolve.” — Enos Mills
+            Created 2023. 
         </p>
-
       </footer>
     </div>
   );
