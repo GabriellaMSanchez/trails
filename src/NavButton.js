3cf5c35f98b40d0f98c26dc44eb0e9bfd1211d21
Gabriella M Sanchez <83431411+GabriellaMSanchez@users.noreply.github.com>
2023-10-01 20:40:45 -0700

    Add in bootstrap CSS  library to MainNavPage
    Imported bootstrap CSS library, configured Main Nav Page buttons, background image and visual formatting.

--- src/NavButton.js
+++ src/NavButton.js
@@ -1,11 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Button from 'react-bootstrap/Button';
 
 function NavButton({ iconName, text }) {
 
     return ( 
         <div>
-            <FontAwesomeIcon icon={iconName}/>
-            {text}
+            <Button className="main-nav-button" variant="dark">
+                <FontAwesomeIcon className="nav-button-img" icon={iconName}/>
+                <div className="nav-button-text">
+                    {text}
+                </div>
+            </Button>
         </div>
     );
 }
